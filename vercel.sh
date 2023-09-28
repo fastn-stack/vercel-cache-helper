62bfb15d3ce35345cd74846d6bfd59350519e9fa
Harsh Singh <harshsingh.js@gmail.com>
2023-09-28 16:53:32 +0530

    update the path variables

--- vercel.sh
+++ vercel.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
+# Set custom directory where binaries will be downloaded and placed in the PATH
+BIN_DIR="$HOME/bin"
+
 # Function to download a binary from a URL and make it executable
 download_binary() {
     URL=$1
     BINARY_NAME=$2
 
     # Download the binary
-    curl -L -o "$BINARY_NAME" "$URL"
+    curl -L -o "$BIN_DIR/$BINARY_NAME" "$URL"
     
     # Make the binary executable
-    chmod +x "$BINARY_NAME"
-    
-    # Move it to a directory in your PATH
-    mv "$BINARY_NAME" /usr/local/bin/
+    chmod +x "$BIN_DIR/$BINARY_NAME"
 }
+
+# Ensure the custom directory exists
+mkdir -p "$BIN_DIR"
 
 # Download fastn binary
 fastn_url=$(curl -s "https://api.github.com/repos/fastn-stack/fastn/releases/latest" | grep -oP '"browser_download_url": "\K(https://.*fastn_linux_musl_x86_64)"')
@@ -22,6 +25,10 @@ download_binary "$fastn_url" "fastn_linux_musl_x86_64"
 # Download vercel-cache-helper binary
 vercel_cache_url=$(curl -s "https://github.com/fastn-stack/vercel-cache-helper/releases/latest" | grep -oP '"browser_download_url": "\K(https://.*vercel-cache-helper_linux_musl_x86_64)"')
 download_binary "$vercel_cache_url" "vercel-cache-helper_linux_musl_x86_64"
+
+# Add the custom directory to your PATH
+echo "export PATH=\$PATH:$BIN_DIR" >> "$HOME/.bashrc"
+source "$HOME/.bashrc"
 
 # Run vercel-cache-helper download
 vercel-cache-helper download
